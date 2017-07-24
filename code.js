45d9675c85913bebd81566198ae0c18926f6b50c
szaroket <mpiestrzynska@interia.pl>
2017-07-24 15:39:47 +0200

    Add ball to canvas

--- code.js
+++ code.js
@@ -20,10 +20,19 @@ function drawEverything() {
     colorRect(0, 0, canvas.width, canvas.height,'black')
     colorRect(10, 200, 20, 150,'white')
     colorRect(770, 200, 20, 150, 'white')
+
+    colorCircle(50, 100, 10, 'white');
 }
 function colorRect(x, y, width, height, color) {
     canvasContext.fillStyle = color;
     canvasContext.fillRect(x, y, width, height);
+}
+
+function colorCircle(x, y, radius, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.beginPath();
+    canvasContext.arc(x, y, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
 }
 
 function callBoth() {
