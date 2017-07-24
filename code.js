b95f5820c6055d260204603e1da8f19c6d0e0a34
szaroket <mpiestrzynska@interia.pl>
2017-07-24 13:53:51 +0200

    Add refresh function

--- code.js
+++ code.js
@@ -2,14 +2,23 @@
 
 var canvas;
 var canvasContext;
+var ballX = 50;
 
 window.onload = function () {
-    drawEverything();
+    var fps = 30;
+
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+
+    // Refresh window
+    setInterval(drawEverything, 1000 / fps);
 }
 
 function drawEverything() {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+    ballX = ballX + 50;
+
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(ballX, 200, 30, 30)
 }
